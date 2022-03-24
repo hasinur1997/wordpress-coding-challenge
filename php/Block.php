891ac0cc1dd9758c4cc51d9e819d9c8737f04f8c
Hasinur Rahman <hasinurrahman3@gmail.com>
2022-03-24 22:43:33 +0600

    fix: Coding improvements

--- php/Block.php
+++ php/Block.php
@@ -54,7 +54,7 @@ class Block {
 		);
 	}
 
-	/**
+    /**
 	 * Renders the block.
 	 *
 	 * @param array    $attributes The attributes for the block.
@@ -63,62 +63,81 @@ class Block {
 	 * @return string The markup of the block.
 	 */
 	public function render_callback( $attributes, $content, $block ) {
-		$post_types = get_post_types(  [ 'public' => true ] );
+		$post_types = get_post_types( array( 'public' => true ) );
 		$class_name = $attributes['className'];
 		ob_start();
 
 		?>
-        <div class="<?php echo $class_name; ?>">
+		<div class="<?php echo esc_attr( $class_name ); ?>">
 			<h2>Post Counts</h2>
 			<ul>
 			<?php
 			foreach ( $post_types as $post_type_slug ) :
-                $post_type_object = get_post_type_object( $post_type_slug  );
-                $post_count = count(
-                    get_posts(
-						[
-							'post_type' => $post_type_slug,
+				$post_type_object = get_post_type_object( $post_type_slug );
+				$post_count       = count(
+					get_posts(
+						array(
+							'post_type'      => $post_type_slug,
 							'posts_per_page' => -1,
-						]
+						)
 					)
-                );
+				);
 
 				?>
-				<li><?php echo 'There are ' . $post_count . ' ' .
-					  $post_type_object->labels->name . '.'; ?></li>
-			<?php endforeach;	?>
-			</ul><p><?php echo 'The current post ID is ' . $_GET['post_id'] . '.'; ?></p>
+				<li>
+					<?php
+						printf(
+							/* translators: 1: Number of Post, 2: Post type name. */
+							esc_html__( 'There are %1$s %2$s.', 'brodo' ),
+							esc_attr( $post_count ),
+							esc_attr( $post_type_object->labels->name )
+						);
+					?>
+				</li>
+			<?php endforeach; ?>
+			</ul>
+				<p>
+					<?php
+						$post_id = ! empty( $_GET['post_id'] ) ? intval( $_GET['post_id'] ) : 0;
+						/* translators: 1: Post ID. */
+						printf( esc_html__( 'The current post ID is %s.', 'brodo' ), esc_attr( $post_id ) );
+					?>
+				</p>
 
 			<?php
-			$query = new WP_Query(  array(
-				'post_type' => ['post', 'page'],
-				'post_status' => 'any',
-				'date_query' => array(
-					array(
-						'hour'      => 9,
-						'compare'   => '>=',
+			$query = new WP_Query(
+				array(
+					'post_type'     => array( 'post', 'page' ),
+					'post_status'   => 'any',
+					'date_query'    => array(
+						array(
+							'hour'    => 9,
+							'compare' => '>=',
+						),
+						array(
+							'hour'    => 17,
+							'compare' => '<=',
+						),
 					),
-					array(
-						'hour' => 17,
-						'compare'=> '<=',
-					),
-				),
-                'tag'  => 'foo',
-                'category_name'  => 'baz',
-				  'post__not_in' => [ get_the_ID() ],
-			));
+					'tag'           => 'foo',
+					'category_name' => 'baz',
+					'post__not_in'  => array( get_the_ID() ),
+				)
+			);
 
 			if ( $query->have_posts() ) :
 				?>
-				 <h2>5 posts with the tag of foo and the category of baz</h2>
-                <ul>
-                <?php
+					<h2><?php esc_html_e( '5 posts with the tag of foo and the category of baz', 'brodo' ); ?></h2>
+				<ul>
+				<?php
 
-                 foreach ( array_slice( $query->posts, 0, 5 ) as $post ) :
-                    ?><li><?php echo $post->post_title ?></li><?php
+				foreach ( array_slice( $query->posts, 0, 5 ) as $post ) :
+					?>
+					<li><?php echo esc_attr( $post->post_title ); ?></li>
+					<?php
 				endforeach;
 			endif;
-		 	?>
+			?>
 			</ul>
 		</div>
 		<?php
